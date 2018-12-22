a1807bb476ac6c8f41c61e3823a5aac9fdaaa590
Ashrya <ashryaagr@gmail.com>
2018-12-22 17:50:39 +0530

    Basic functionality added

--- Checkmate2019/Base/models.py
+++ Checkmate2019/Base/models.py
@@ -6,8 +6,10 @@ import re
 class Team(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     team_name = models.CharField(max_length=25)
-    password = models.CharField(default="abcde12345", max_length=20)
-    score = models.IntegerField(default=0, max_length=10)
+    password = models.CharField(max_length=20)
+    score = models.IntegerField(default=0)
+    rank = models.IntegerField(default=0)
+    puzzles_solved = models.IntegerField(default=0)
     ip_address = models.CharField(null=True, max_length=20)
     
     def __str__(self):
@@ -15,10 +17,10 @@ class Team(models.Model):
 
 
 class Member(models.Model):
-    id = models.CharField(max_length=13, primary_key=True, validators=[
+    id =models.CharField(max_length=13, primary_key=True, validators=[
         validators.RegexValidator(re.compile('^201[5-8]{1}[0-9A-Z]{4}[0-9]{4}P$'), 
         message='Enter your valid BITS ID, for eg. 2018A7PS0210P')])
-    Team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=False, related_name="Member")
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=False, related_name="Member")
     
     def __str__(self):
-        return f"{self.Team} : {self.id}"
+        return f"{self.team} : {self.id}"
--- Checkmate2019/Base/urls.py
+++ Checkmate2019/Base/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    
+    path("sign_up_team", views.sign_up_team , name="sign_up_team"),
+    path("sign_up_Member", views.sign_up_Member, name="sign_up_Member"), 
+    path("sign_in", views.sign_in, name="sign_in"),
+    path("sign_out", views.sign_out, name="sign_out"),
 ]
