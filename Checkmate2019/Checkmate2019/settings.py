aa84810577d38cb5b205675c3bc322c0d8424755
Saujas Adarkar <saujas.adarkar@gmail.com>
2019-02-22 19:37:33 +0530

    Debug set true
    Error on debug=false, while loading static files

--- Checkmate2019/Checkmate2019/settings.py
+++ Checkmate2019/Checkmate2019/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v^v6g*71tx8rs92oc$opf9+a4ix5vitf7u8g&@8hp-*4nk866='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
